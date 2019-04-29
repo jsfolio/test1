052c6e0d4e6a36b4302db741eb2224eb56ed50cc
jsfolio <jhm8540@daum.net>
2019-04-29 09:37:42 +0900

    0429 update

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var chk = false;
     $('.menu_box').click(function(){
-        if(chk == true) { //메뉴닫기
+        if(chk == true) { 
             $('.menu_box').removeClass('active'); 
             $('.menu_box').addClass('menu_box_reverse');  
             $('.panel-overlay').css({
@@ -9,7 +9,7 @@ $(document).ready(function(){
             })
             chk = false; 
         }
-        else { //메뉴열기
+        else {
             $('.menu_box').removeClass('menu_box_reverse');
             $('.menu_box').addClass('active');
             $(".mobile_menu").removeClass("hide");
@@ -34,20 +34,6 @@ $(document).ready(function(){
         chk = false; 
             
     });
-
-    // var winWidth = $(window).width();
-    // if(winWidth >= 1024){
-    //     $('.panel-overlay').trigger('click')
-    //     chk = false; 
-     
-    // }else{
-    //     $('.panel-overlay').css({
-    //         display : 'block'
-    //     })
-    //     chk = true;
-    // }
-   
-    //서브메뉴
 
     $('.mobile_menu ul li').click(function(){
         $(this).children('.sub_hide').stop().slideToggle();
